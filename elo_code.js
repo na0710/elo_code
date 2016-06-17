1a01cc7ca24fa9a5fbc3cf3ca1153d5665ab784a
Nisha Agarwal <na07101997@gmail.com>
2016-06-17 11:28:24 +0530

    elo_code.js

--- elo_code.js
+++ elo_code.js
@@ -1,16 +1,30 @@
- var act_scr1=1;
- var act_scr2=0;
- var r1=1000;
- var r2=2000;
+  var act_scr1;
+ var act_scr2;
+ var r1;
+ var r2;
+       function update_ratings(r1,r2,winner)
+        {
 
  	var exp_scr1=Math.pow(10,(r1/400))/(Math.pow(10,(r1/400))+Math.pow(10,(r2/400)));
  	var exp_scr2=Math.pow(10,(r2/400))/(Math.pow(10,(r1/400))+Math.pow(10,(r2/400)));
+        if (winner=1)
+        { act_scr1=1;
+          act_scr2=0;}
+        else if (winner=2)
+        {  act_scr=0;
+            act_scr2=1; }
+        else 
+        { act_scr1=act_scr2=0.5}
+        
  	r1=r1+32*(act_scr1-exp_scr1);
  	r2=r2+32*(act_scr2-exp_scr2);
- 	
+        var ratings=[r1,r2];
+        return ratings;
+ 	}
 function display_rates(){
-document.writeln("new score of player1 = "+r1);
+  var rating_array = update_ratings(1000,2000,1);
+document.writeln("new score of player1 = "+rating_array[0]);
 document.writeln("<br></br>");
-document.writeln("new score of player2 = "+r2);
+document.writeln("new score of player2 = "+rating_array[1]);
 
 }
